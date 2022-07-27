bdd3c86588a4c751a13d1882e669c9a6fe89ec77
jiabaocheng <baocheng.jia@pengyunnetwork.com>
2022-07-27 11:20:35 +0800

    leet code

---
+++ ulrica-leetcode/src/main/java/com/et/ulrica/serach/SearchAlgorithm.java
@@ -0,0 +1,64 @@
+package com.et.ulrica.serach;
+
+/**
+ * @author:ulrica
+ * @date: 2022/7/27 上午9:44
+ * @description:
+ * @version:1.0
+ **/
+
+/**
+ *
+ */
+public class SearchAlgorithm {
+
+
+  /**
+   * link{https://leetcode.cn/problems/binary-search/} ideas: Binary search is mainly suitable for
+   * an ascending array.Firstly , used right 、left record this search range, left <= right for
+   * search all num.And mid record the middle index of number array, check number with target.If
+   * nums[mid] == target , we can get search result and return index, else if nums[mid] > target, we
+   * should reset right = mid -1,or else we should reset left = mid + 1.
+   *
+   * @param nums
+   * @param target
+   * @return
+   */
+  public static int binarySearch(int[] nums, int target) {
+    int right = nums.length - 1;
+    int left = 0;
+    int mid = left + (right - left) / 2;
+    while (left <= right) {
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+      mid = left + (right - left) / 2;
+    }
+    return -1;
+  }
+
+  public static int searchInsert(int[] nums, int target) {
+    int right = nums.length - 1;
+    int left = 0;
+    int mid = 0;
+    while (left <= right) {
+      mid = left + (right - left)/2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    if(nums[mid] > target){
+      return mid;
+    }else{
+      return mid + 1;
+    }
+  }
+}
